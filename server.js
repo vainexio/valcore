de97461c5727fa774c8dd18f1a7de2a43036f9a8
Ian <111634400+vainexio@users.noreply.github.com>
2026-01-07 21:03:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const Discord = require('discord.js');
 const { joinVoiceChannel } = require('@discordjs/voice');
 const { WebhookClient, Permissions, Client, Intents, MessageEmbed, MessageActionRow, MessageButton, MessageSelectMenu } = Discord;
 const myIntents = new Intents();
-myIntents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES);
+//myIntents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES);
+myIntents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES);
+
 const client = new Client({ intents: myIntents, partials: ["CHANNEL"] });
 
 // Env
